37ad249530dc284594235030f8f44001e22cfe22
Fernando de Castro <fca.apps@gmail.com>
2021-07-19 00:54:53 -0300

    Rodando a aplicação

--- CrudFornec/Startup.cs
+++ CrudFornec/Startup.cs
@@ -26,6 +26,8 @@ namespace CrudFornec
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
