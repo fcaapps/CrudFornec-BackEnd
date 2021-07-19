f1cd1d3b38a6f0d267aff4bf8eaa2439d9f29595
Fernando de Castro <fca.apps@gmail.com>
2021-07-19 02:22:59 -0300

    Criação da Migration da Tabela Fornecedor

---
+++ CrudFornec.Data/Context/CrudFornecContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+//using CrudFornec.Data.Extensions;
+//using CrudFornec.Data.Mappings;
+using CrudFornec.Domain.Entities;
+
+namespace CrudFornec.Data.Content
+{
+    public class CrudFornecContext : DbContext
+    {
+        public CrudFornecContext(DbContextOptions<CrudFornecContext> option)
+            : base(option) {}
+
+        #region "DbSets"
+        public DbSet<Fornecedor> Fornecedor { get; set; }
+        #endregion
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfiguration(new UserMap());
+
+            //modelBuilder.ApplyGlobalConfigurations();
+            //modelBuilder.SeedData();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
